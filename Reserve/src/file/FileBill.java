676bbe8ff3e18a0237638282806fade179d66afc
dinb1242 <dinb1242@naver.com>
2018-05-31 14:41:45 +0900

    Modified by Jeong

--- Reserve/src/file/FileBill.java
+++ Reserve/src/file/FileBill.java
@@ -3,14 +3,23 @@ package file;
 
 
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class FileBill extends JFrame{
+public class FileBill extends JFrame implements ActionListener{
 	
-	private JPanel pTest;
-	private JLabel lblTest;
+	private JPanel p1;
+	private JLabel lblDivision, lblContent;
+	private JComboBox<String> cbDivision, cbContent;
+	private JButton btnSearch, btnReset;
+	private String[] cbStr = {"거래서", "품목", "발주 리스트"};
 	
 	public FileBill()
 	{
@@ -19,6 +28,39 @@ public class FileBill extends JFrame{
 		//setLocationRelativeTo(null);
 		setSize(500, 500);
 		
+		// Initialization panels, labels and buttons
+		p1 = new JPanel(new FlowLayout(FlowLayout.CENTER,5,10));
+		lblDivision = new JLabel("구분");
+		lblContent = new JLabel("내용");
+		cbDivision = new JComboBox();
+		cbContent = new JComboBox();
+		btnSearch = new JButton("검색");
+		btnReset = new JButton("초기화");
+		
+		// initialization combo box
+		
+		for(int i = 0 ; i < cbStr.length ; i++)
+		{
+			cbDivision.addItem(cbStr[i]);
+		}
+		
+		
+		// Adding components to p1
+		p1.add(lblDivision);
+		p1.add(cbDivision);
+		p1.add(lblContent);
+		p1.add(cbContent);
+		p1.add(btnSearch);
+		p1.add(btnReset);
+		
+		// JPanel 1
+		add(p1);
+		
 		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
