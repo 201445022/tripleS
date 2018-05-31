204433e64bfa90cc30daa62aca5c58574a7937fe
Administrator <Administrator@121.172.165.220>
2018-05-31 14:12:45 +0900

    jcw 1

--- Reserve/src/Main.java
+++ Reserve/src/Main.java
@@ -1,120 +1,101 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-import file.FileBill;
+import register.RegiClient;
 
-public class Main extends JFrame implements ActionListener{
-   
-   private JMenuBar mb;
-   private JMenu fileMenu, listMenu, registerMenu, inquiryMenu;
-   private JMenuItem f_bill, f_exit; // FileMenu
-   private JMenuItem l_client, l_item, l_order; // ListMenu
-   private JMenuItem r_client, r_item, r_orderList; // RegisterMenu
-   private JMenuItem i_faq, i_obo;
-   public Main() {
-      // JFrame
-      setTitle("물품 주문 관리");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      //setLocationRelativeTo(null);
-      setSize(500, 500);
-      // JMenuBar
-      mb = new JMenuBar();
-      setJMenuBar(mb);
-      // JMenu
-      fileMenu = new JMenu("파일");
-      listMenu = new JMenu("리스트");
-      registerMenu = new JMenu("등록");
-      inquiryMenu = new JMenu("문의");
-      mb.add(fileMenu);
-      mb.add(listMenu);
-      mb.add(registerMenu);
-      mb.add(inquiryMenu);
-      
-      //File Menu
-      f_exit = new JMenuItem("종료");
-      f_bill = new JMenuItem("내역서");
-      f_exit.addActionListener(this);
-      f_bill.addActionListener(this);
-      fileMenu.add(f_bill);
-      fileMenu.add(f_exit);
+public class Main extends JFrame implements ActionListener {
 
-      // List Menu
-      l_client = new JMenuItem("거래처");
-      l_item = new JMenuItem("품목");
-      l_order = new JMenuItem("주문");
-      listMenu.add(l_client);
-      listMenu.add(l_item);
-      listMenu.add(l_order);
-      // Register Menu
-      r_client = new JMenuItem("거래처");
-      r_item = new JMenuItem("품목");
-      r_orderList = new JMenuItem("주문내역");
-      registerMenu.add(r_client);
-      registerMenu.add(r_item);
-      registerMenu.add(r_orderList);
-      // InquiryMenu
-      i_faq = new JMenuItem("FAQ");
-      i_obo = new JMenuItem("1:1문의");
-      inquiryMenu.add(i_faq);
-      inquiryMenu.add(i_obo);
-      // Visible
-      setVisible(true);
-   }
+	private JMenuBar mb;
+	private JMenu fileMenu, listMenu, registerMenu, inquiryMenu;
+	private JMenuItem f_bill, f_exit; // FileMenu
+	private JMenuItem l_client, l_item, l_order; // ListMenu
+	private JMenuItem r_client, r_item, r_orderList; // RegisterMenu
+	private JMenuItem i_faq, i_obo;
 
-   public static void main(String[] args) {
-      new Main();
-   }
+	public Main() {
+		// JFrame
+		setTitle("물품 주문 관리");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// setLocationRelativeTo(null);
+		setSize(500, 500);
+		// JMenuBar
+		mb = new JMenuBar();
+		setJMenuBar(mb);
+		// JMenu
+		fileMenu = new JMenu("파일");
+		listMenu = new JMenu("리스트");
+		registerMenu = new JMenu("등록");
+		inquiryMenu = new JMenu("문의");
+		mb.add(fileMenu);
+		mb.add(listMenu);
+		mb.add(registerMenu);
+		mb.add(inquiryMenu);
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	Object obj = e.getSource();
+		// File Menu
+		f_exit = new JMenuItem("종료");
+		f_bill = new JMenuItem("내역서");
+		f_exit.addActionListener(this);
+		f_bill.addActionListener(this);
+		fileMenu.add(f_bill);
+		fileMenu.add(f_exit);
+
+		// List Menu
+		l_client = new JMenuItem("거래처");
+		l_item = new JMenuItem("품목");
+		l_order = new JMenuItem("주문");
+		listMenu.add(l_client);
+		listMenu.add(l_item);
+		listMenu.add(l_order);
+		// Register Menu
+		r_client = new JMenuItem("거래처");
+		r_item = new JMenuItem("품목");
+		r_orderList = new JMenuItem("주문내역");
+		registerMenu.add(r_client);
+		registerMenu.add(r_item);
+		registerMenu.add(r_orderList);
+		// InquiryMenu
+		i_faq = new JMenuItem("FAQ");
+		i_obo = new JMenuItem("1:1문의");
+		inquiryMenu.add(i_faq);
+		inquiryMenu.add(i_obo);
+		// Visible
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
 	
-	if(obj == f_exit)
-	{
-		System.exit(0);
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object obj = e.getSource();
+
+		if (obj == f_exit) {
+			System.exit(0);
+		} else if (obj == f_bill) {
+
+		} else if (obj == l_client) {
+			this.dispose();
+			new RegiClient();
+		} else if (obj == l_item) {
+
+		} else if (obj == l_order) {
+
+		} else if (obj == r_client) {
+
+		} else if (obj == r_item) {
+
+		} else if (obj == r_orderList) {
+
+		} else if (obj == i_faq) {
+
+		} else if (obj == i_obo) {
+
+		}
 	}
-	else if (obj == f_bill)
-	{
-		
-	}
-	else if (obj == l_client)
-	{
-		
-	}
-	else if (obj == l_item)
-	{
-		
-	}
-	else if (obj == l_order)
-	{
-		
-	}
-	else if (obj == r_client)
-	{
-		
-	}
-	else if (obj == r_item)
-	{
-		
-	}
-	else if (obj == r_orderList)
-	{
-		
-	}
-	else if (obj == i_faq)
-	{
-		
-	}
-	else if (obj == i_obo)
-	{
-		
-	}
-}
 
 }
--- Reserve/src/register/RegiClient.java
+++ Reserve/src/register/RegiClient.java
@@ -1,16 +1,23 @@
 package register;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class RegiClient extends JFrame{
-
+	private JLabel lblCompany, lblManager, lblTel, lblFax, lblEmail;
+	private JTextField tfCompany, tfManager;
+	private JTextField tfTel_1, tfTel_2, tfTel_3;
+	private JTextField tfFax_1, tfFax_2, tfFax_3;
+	private JTextField tfEmail_1, tfEmail_2; 
 	public RegiClient()
 	{
-		setTitle("물품 주문 관리");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setLocationRelativeTo(null);
+		setTitle("거래처 등록");
 		setSize(500, 500);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setLocationRelativeTo(null);
 		
+		// Visible
 		setVisible(true);
 	}
 }
